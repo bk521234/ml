0b6ad0ab11ac9b32085339fe68d6bb732e34a6ca
PSSEA\bkielpinski <bryan.kielpinski52@gmail.com>
2019-06-03 15:18:33 -0600

    adding basic vectorizing example file.

---
+++ basic_vectorizer.py
@@ -0,0 +1,28 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+# list of text documents
+text = ["The quick brown fox jumped over the lazy dog."]
+
+# create the transform
+vectorizer = CountVectorizer()
+
+# tokenize and build vocab
+vectorizer.fit(text)
+
+# summarize
+print(vectorizer.vocabulary_)
+
+# encode document
+vector = vectorizer.transform(text)
+
+# summarize encoded vector
+print(vector.shape)
+print(type(vector))
+print(vector.toarray())
+
+# encode another document
+text2 = ["the brown puppy"]
+
+vector = vectorizer.transform(text2)
+print(vector.toarray())
+
